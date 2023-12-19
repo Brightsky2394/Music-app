9c22f2a5808924dc854e810ec630e0693cd8bef7
Tinsel19 <olaoluwaoladunjoye130@gmail.com>
2023-12-19 21:49:48 +0100

    pull from main branch

--- musical-app/src/components/Utils/Spotify.js
+++ musical-app/src/components/Utils/Spotify.js
@@ -17,7 +17,7 @@ const Spotify = {
             window.history.pushState("Access Token", null, "/");
             return accessToken;
         } else {
-            const accessUrl = "https://accounts.spotify.com/authorize?client_id=${clientid}&response_type=token&scope=playlist-modify-public&redirect_url=${redirectUrl}";
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_url=${redirectUrl}`;
             window.location = accessUrl;
         }
     },
